842a250e2ef40c9233cb544285ea8fcef94c2a3e
khowellmobile <khowellmobile@gmail.com>
2026-01-08 12:21:24 -0500

    Changed up header and menu in the template

--- frontend/src/layouts/layout-sections/Header.jsx
+++ frontend/src/layouts/layout-sections/Header.jsx
@@ -1,9 +1,25 @@
 import classes from "./Header.module.css";
 
+import ReactIcon from "../../assets/react.svg";
+import SearchBox from "../../components/utilities/SearchBox";
+
 const Header = () => {
     return (
         <div className={classes.mainContainer}>
-            <p>Header</p>
+            <div className={classes.left}>
+                <div className={classes.icon}>
+                    <img src={ReactIcon} alt="Icon" />
+                </div>
+                <p>
+                    <strong>React</strong>App
+                </p>
+                <SearchBox type="text" name="search" value={""} placeholder={"Search"} />
+            </div>
+            <div className={classes.right}>
+                <div className={classes.divIcon}>
+                    <p>W</p>
+                </div>
+            </div>
         </div>
     );
 };
--- frontend/src/layouts/layout-sections/Menu.jsx
+++ frontend/src/layouts/layout-sections/Menu.jsx
@@ -2,25 +2,27 @@ import classes from "./Menu.module.css";
 
 import MenuLineItem from "../../components/utilities/MenuLineItem";
 
+import ReactIcon from "../../assets/react.svg";
+
 const Menu = () => {
     return (
         <div className={classes.mainContainer}>
             <section className={classes.itemsSection}>
                 <div className={classes.menuHeader}>ANALYTICS</div>
-                <MenuLineItem itemName="Link 1" link="/app/home" icon={<div></div>} />
-                <MenuLineItem itemName="Link 2" link="/app/transactions" icon={<div></div>} />
-                <MenuLineItem itemName="Link 3" link="/app/rents" icon={<div></div>} />
+                <MenuLineItem itemName="Link 1" link="/" icon={<img src={ReactIcon} alt="Icon" />} />
+                <MenuLineItem itemName="Link 2" link="/" icon={<img src={ReactIcon} alt="Icon" />} />
+                <MenuLineItem itemName="Link 3" link="/" icon={<img src={ReactIcon} alt="Icon" />} />
                 <div className={classes.menuHeader}>ACCOUNT</div>
-                <MenuLineItem itemName="Special Link 1" link="/app/properties" icon={<div></div>} />
+                <MenuLineItem itemName="Special Link 1" link="/" icon={<img src={ReactIcon} alt="Icon" />} />
                 <div className={classes.menuHeader}>APPLICATION</div>
-                <MenuLineItem itemName="Support" link="/app/support" icon={<div></div>} />
-                <MenuLineItem itemName="Settings" link="/app/settings" icon={<div></div>} />
+                <MenuLineItem itemName="Support" link="/" icon={<img src={ReactIcon} alt="Icon" />} />
+                <MenuLineItem itemName="Settings" link="/" icon={<img src={ReactIcon} alt="Icon" />} />
             </section>
             <MenuLineItem
                 className={classes.logoutItem}
                 itemName="Logout"
                 link="/"
-                icon={<div></div>}
+                icon={<img src={ReactIcon} alt="Icon" />}
                 onClick={() => {}}
             />
         </div>
