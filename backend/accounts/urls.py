d703f8328b0be8cf8f9c7b6b947eaead334b65c3
khowellmobile <khowellmobile@gmail.com>
2026-04-20 13:11:46 -0400

    Adding client.jsx and prepped backed for switch to Httponly cookies

---
+++ backend/accounts/urls.py
@@ -0,0 +1,11 @@
+# rental_api/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('auth/login/', views.CookieTokenObtainPairView.as_view(), name='cookie-login'),
+    path('auth/refresh/', views.CookieTokenRefreshView.as_view(), name='cookie-refresh'),
+    path('auth/logout/', views.CookieLogoutView.as_view(), name='cookie-logout'),
+    path('auth/jwt/create/', views.CookieTokenObtainPairView.as_view(), name='cookie-jwt-create'),
+    path('auth/jwt/refresh/', views.CookieTokenRefreshView.as_view(), name='cookie-jwt-refresh'),
+]
